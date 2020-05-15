ad22b309e83074e1de8fef5c9eca627a1eeac998
MinexAutomation <minex.automation@gmail.com>
2020-05-15 10:26:10 -0700

    Added R5T.D0007.D0005 implementation library.

---
+++ source/R5T.D0007.D0005/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+
+
+namespace R5T.D0007.D0005
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="ProgramNameDirectoryNameProvider"/> implementation of <see cref="IProgramNameDirectoryNameProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddD0005ProgramNameDirectoryNameProvider(this IServiceCollection services)
+        {
+            services.AddSingleton<IProgramNameDirectoryNameProvider, ProgramNameDirectoryNameProvider>();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="ProgramNameDirectoryNameProvider"/> implementation of <see cref="IProgramNameDirectoryNameProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static ServiceAction<IProgramNameDirectoryNameProvider> AddD0005ProgramNameDirectoryNameProviderAction(this IServiceCollection services)
+        {
+            var serviceAction = ServiceAction<IProgramNameDirectoryNameProvider>.New(() => services.AddD0005ProgramNameDirectoryNameProvider());
+            return serviceAction;
+        }
+    }
+}
---
+++ source/R5T.D0007.D0005/Code/Services/Implementations/ProgramNameDirectoryNameProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using R5T.D0005;
+
+
+namespace R5T.D0007.D0005
+{
+    public class ProgramNameDirectoryNameProvider : IProgramNameDirectoryNameProvider
+    {
+        private IProgramNameProvider ProgramNameProvider { get; }
+
+
+        public ProgramNameDirectoryNameProvider(IProgramNameProvider programNameProvider)
+        {
+            this.ProgramNameProvider = programNameProvider;
+        }
+
+        public async Task<string> GetProgramNameDirectoryNameAsync()
+        {
+            var programName = await this.ProgramNameProvider.GetProgramNameAsync();
+
+            var programNameDirectoryName = programName; // Assume program name requires no changes to be the program name directory name.
+            return programNameDirectoryName;
+        }
+    }
+}
