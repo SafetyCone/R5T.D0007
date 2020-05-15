529055ad8907a582b5f130c4b0e34d1c7fcc7294
MinexAutomation <minex.automation@gmail.com>
2020-05-15 10:18:37 -0700

    Added R5T.D0007.Base library. Added IProgramNameDirectoryNameProvider service interface definition.

---
+++ source/R5T.D0007.Base/Code/Services/Definitions/IProgramNameDirectoryNameProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+
+namespace R5T.D0007
+{
+    public interface IProgramNameDirectoryNameProvider
+    {
+        Task<string> GetProgramNameDirectoryNameAsync();
+    }
+}
